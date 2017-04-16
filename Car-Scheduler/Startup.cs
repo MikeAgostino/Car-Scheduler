d6e1497884debc4fd4d78441c9d8aaf44acad1b1
Michael Agostino <mike.agostino92@gmail.com>
2017-04-16 15:00:00 -0400

    Added SystemWeb OWIN Added startup class for any pre startup config

---
+++ Car-Scheduler/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(API.Startup))]
+namespace API
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+           //Start up config goes here
+        }
+    }
+}
